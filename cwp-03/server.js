d8633ff11d7e421d61766024768cecfd16ea1264
Yurij Bogdan <fogolan@gmail.com>
2017-10-10 00:16:24 +0300

    added get files functionality

--- cwp-03/server.js
+++ cwp-03/server.js
@@ -5,15 +5,28 @@ const constants = require('./modules/constants_module');
 class Server {
     constructor(maxNumberOfClients) {
         this.clients = [];
+        this.filesChanks = [];
         this.maxClients = maxNumberOfClients;
     }
 
     request(data, client) {
+        console.log('from client: ', data.toString());
         if(client.id != undefined) {
-            return;
+            console.log((this.ClientDialogFILES(data)) ? constants.sendNextFile : constants.error);
+            return (this.ClientDialogFILES(data)) ? constants.sendNextFile : constants.error;
         }
         let result = this.checkInitMessage(data, client);
         return result;
+    }
+
+    ClientDialogFILES(data, client) {
+        let bufferChank = Buffer.from(data);
+        this.filesChanks[client.id].push(bufferChank);
+
+        if (data.toString().endsWith(endSendingFile)) {
+            createFileFromBinData(client.id);
+            return true;
+        }
     }
 
     checkInitMessage(data, client) {
@@ -30,6 +43,7 @@ class Server {
     tryConnectClient(client) {
         if(this.clients.length < this.maxClients) {
             client.id = uuidv4();
+            console.log('new client: ', client.id);
             this.clients.push(client);
             return true;
         }
