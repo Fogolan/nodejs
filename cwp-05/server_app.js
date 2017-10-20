715c5e453fd1896dd7b3f31984d6ffbdbe83c20c
Yurij Bogdan <fogolan@gmail.com>
2017-10-20 20:30:46 +0300

    include controllers and root mechanism into application

--- cwp-05/server_app.js
+++ cwp-05/server_app.js
@@ -2,14 +2,24 @@ const http = require('http');
 const fs = require('fs');
 
 const constants = require('./modules/constants_module');
+const ArticleController = require('./constollers/articleController');
+const RootMechanism = require('./constollers/rootMechanism');
+
+let controllers = [new ArticleController(),];
+let rootMechanism = new RootMechanism(controllers);
+
 
 http.createServer(function(request, response){
     
-   console.log("Url: " + request.url);
-   console.log("Тип запроса: " + request.method);
-   console.log("User-Agent: " + request.headers["user-agent"]);
-   console.log("Все заголовки");
-   console.log(request.headers);
+//    console.log("Url: " + request.url);
+//    console.log("Тип запроса: " + request.method);
+//    console.log("User-Agent: " + request.headers["user-agent"]);
+//    console.log("Все заголовки");
+//    console.log(request.headers);
+
+    let handler = rootMechanism.getHandler(request.url, request.method);
+
+    console.log(handler);
     
    response.end();
 }).listen(constants.port);
